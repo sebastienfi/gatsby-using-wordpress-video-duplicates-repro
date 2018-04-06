0f76629f77e01727cda730c277f7ecec3327ea55
Sebastien Fichot <fichot.sebastien@gmail.com>
2018-04-06 17:33:06 +0200

    Updtated gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
+        name: `localMedias`,
         path: `${__dirname}/.cache/gatsby-source-filesystem`
       }
     },
